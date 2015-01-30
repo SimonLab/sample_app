b4f7e45e3316620d7eba36b82307bbe5b24123e3
SimonLab <simon.labondance@gmail.com>
2015-01-30 12:24:47 +0000

    Finish login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   get 'signup' => 'users#new'
-
   get 'help' => 'static_pages#help'
-
   get 'about' => 'static_pages#about'
-
   get 'contact' => 'static_pages#contact'
-
   resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
